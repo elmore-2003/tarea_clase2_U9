4928e43b969cd543e6ff6f0db59c59374936c1ec
elmore-2003 <122875816+elmore-2003@users.noreply.github.com>
2024-05-29 18:59:34 +0200

    tercero

--- src/main/java/conexion_clase2/conexion.java
+++ src/main/java/conexion_clase2/conexion.java
@@ -5,15 +5,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class conexion {
-    private static final String URL = "jdbc:mysql://localhost:3306/testdb";
-    private static final String USER = "user";
-    private static final String PASSWORD = "password";
+    private static final String URL = "jdbc:mysql://localhost:3309/mibasededatos?allowPublicKeyRetrieval=true";
+    private static final String USER = "root";
+    private static final String PASSWORD = "1234";
 
     public Connection getConnection(){
         Connection conexion = null;
             try{
         conexion = DriverManager.getConnection(URL, USER, PASSWORD);
-        System.out.println("Conexión Ok");
+        System.out.println("Conexión establecida");
     } catch (SQLException e) {
         System.out.println("Error en la conexión");
         e.printStackTrace();
