03ad385c098792bafcb0966221c2624ab7538ef0
elmore-2003 <122875816+elmore-2003@users.noreply.github.com>
2024-05-29 17:31:13 +0200

    segundo

--- src/main/java/conexion_clase2/main.java
+++ src/main/java/conexion_clase2/main.java
@@ -1,4 +1,22 @@
 package conexion_clase2;
 
+import java.sql.*;
 public class main {
+    public static void main(String[] args) {
+
+        conexion conexion = new conexion();
+        Connection cn = null;
+        Statement stm = null;
+        ResultSet rs = null;
+
+
+        try {
+            cn = conexion.getConnection();
+            stm = cn.createStatement();
+
+
+        } catch (SQLException e) {
+
+        }
+    }
 }
